201fae0aaf3b86b36d265b9d7fc75927d34f6c67
carmenFischer <carmen.fischer@stud.fh-campuswien.ac.at>
2022-03-10 23:18:03 +0100

    shorter code

--- src/main/java/checkPassword.java
+++ src/main/java/checkPassword.java
@@ -6,7 +6,7 @@ public class checkPassword {
      * at least one special character ()#$?!%/@,
      * no more than two successive numbers,
      * no more than three numbers that are the same right after each other**/
-    public static boolean checkPassword (String password){
+   /* public static boolean checkPassword (String password){
         if(password.length()<8||password.length()>25) {
             return false;
         }
@@ -29,5 +29,17 @@ public class checkPassword {
             return false;
         }
         else return !password.matches(".*(^A-Z|^a-z|^0-9|[^()#$?!%/@$]).*");
+    }*/
+
+    //least amount of statement
+    public static boolean checkPassword (String password){
+        return (password.length()<8||password.length()>25)&&
+                (!password.matches(".*[A-Z].*"))&&
+                (!password.matches(".*[a-z].*"))&&
+                (!password.matches(".*[0-9].*"))&&
+                (!password.matches(".*[()#$?!%/@].*"))&&
+                (password.matches(".*(123|234|345|456|567|678|789).*"))&&
+                (password.matches(".*(1{4,}|2{4,}|3{4,}|4{4,}|5{4,}|6{4,}|7{4,}|8{4,}|9{4,}).*"))&&
+                (!password.matches(".*(^A-Z|^a-z|^0-9|[^()#$?!%/@$]).*"));
     }
 }
